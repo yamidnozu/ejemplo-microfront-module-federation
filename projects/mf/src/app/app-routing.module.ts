5bfee5ad98d7a40f8ef0b36a2063e7cf7e519b16
eynoguer <edilson.noguera@bancolombia.com.co>
2022-11-30 21:54:53 -0500

    Configure project mf

--- projects/mf/src/app/app-routing.module.ts
+++ projects/mf/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    pathMatch: 'full',
+    path: '',
+    component: DefaultComponent
+  },
+  {
+    path: 'mf',
+    loadChildren: () => import('./showinfo/showinfo.module').then(m => m.ShowinfoModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
